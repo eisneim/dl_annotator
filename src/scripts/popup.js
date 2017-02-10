7e82ccace2587b0c6a757941d1b54349d0f99925
eisneim <eisneim@Dimension5.local>
2017-02-10 17:24:42 +0800

    [done] popup page, add and remove classname

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -17,6 +17,25 @@ let defaultConfig = {
   server: "http://localhost:8000/upload",
 }
 
+let $form = document.getElementById("mainForm")
+let $msg = document.getElementById("message")
+let $createClassBtn = document.getElementById("createClassBtn")
+let $newClassName = document.getElementById("newClassName")
+let $confirmBox = document.getElementById("confirmBox")
+
+let timerID
+function notify(...args){
+  if (timerID !== undefined) clearTimeout(timerID)
+
+  let msg = args.join(" ")
+  $msg.innerText = msg
+  $msg.style.visibility = "visible"
+  timerID = setTimeout(() => {
+    $msg.innerText = ""
+    $msg.style.visibility = "hidden"
+  }, 4000)
+}
+
 function initConfig() {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get("localSaveMethod", data => {
@@ -28,7 +47,42 @@ function initConfig() {
   })
 }
 
-let $form = document.getElementById("mainForm")
+function _confirm(msg, cb){
+  $confirmBox.style.visibility = "visible"
+  $confirmBox.firstElementChild.innerText = msg
+  let $cancel = $confirmBox.firstElementChild.nextElementSibling
+  let $confirm = $cancel.nextElementSibling
+  $cancel.onclick = () => {
+    $confirmBox.style.visibility = "hidden"
+  }
+  $confirm.onclick = () => {
+    cb()
+    $confirmBox.style.visibility = "hidden"
+  }
+}
+
+function removeClass(name, elm) {
+  let msg = "are you sure you want to delete " + name + "? prvious annoted data might be currupted!"
+  _confirm(msg, () => {
+    chrome.storage.sync.get("classes", data => {
+      let idx = data.classes.indexOf(name)
+      log("remove:", name, idx)
+      data.classes.splice(idx, 1)
+      chrome.storage.sync.set({ classes: data.classes.slice() })
+      elm.remove()
+    })
+  })
+}
+
+function createClassText(name) {
+  let $span = document.createElement("span")
+  $span.className="dla_classText"
+  $span.innerText = name
+  $span.addEventListener("click", () => {
+    removeClass(name, $span)
+  })
+  return $span
+}
 
 // ant that's why we should use tow-way data-binding or React like render methods
 // it's really cumbersome to map data to html element
@@ -41,16 +95,13 @@ function setFormInitialValue() {
       // set <options>
       config.classes.forEach((cc, idx) => {
         let $opt = document.createElement("option")
-        let $span = document.createElement("span")
-        $span.className="dla_classText"
-
         if (config.defaultClass === idx)
           $opt.selected = true
 
         $opt.value = cc
-        $opt.innerText = $span.innerText = cc
+        $opt.innerText = cc
         $defaultClass.appendChild($opt)
-        $classes.appendChild($span)
+        $classes.appendChild(createClassText(cc))
       })
       // set server address
       let $server = $form.elements.server
@@ -65,6 +116,7 @@ function setFormInitialValue() {
   })
 }
 
+
 $form.addEventListener("change", e => {
   let target = e.target
   let elm = $form.elements
@@ -76,13 +128,38 @@ $form.addEventListener("change", e => {
       log("update defaultClass", target.value, data.classes)
       chrome.storage.sync.set({ defaultClass: data.classes.indexOf(target.value) })
     })
+    notify("default class saved as: ", target.value)
   } else if (target === elm.saveFile) {
     chrome.storage.sync.set({ localSaveMethod: "FILENAME" })
+    notify("local save method saved as: ", "filename")
   } else if (target === elm.saveJSON) {
     chrome.storage.sync.set({ localSaveMethod: "JSON" })
+    notify("local save method saved as: ", "JSON")
   } else if (target === elm.server) {
     chrome.storage.sync.set({ server: target.value })
+    notify("Server address updated!")
   }
+})
+
+$createClassBtn.addEventListener("click", () => {
+  chrome.storage.sync.get("classes", data => {
+    let name = $newClassName.value
+    if (!name) return notify("name should not be empty")
+    if (data.classes.indexOf(name) > -1) {
+      return notify(name, "already exits")
+    }
+    let regx = /[\.\$\!\@\#\%\^\&\*\\\<\>\?\/\~\`\[\]\{\}]/
+    if (regx.test(name))
+      return notify("name should not contain special charactors: .$!@#%^&*<>?/~\\[]{}`")
+
+    let newClasses = data.classes.slice()
+    newClasses.push(name)
+    chrome.storage.sync.set({ classes: newClasses })
+
+    document.getElementById("classes")
+      .appendChild(createClassText(name))
+  })
+
 })
 
 initConfig()
